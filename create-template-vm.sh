00c4d1304a02c5a0a175f5a3215d3701c9b387a6
Savinien Petitjean <savinien.petitjean@gmail.com>
2025-12-04 17:33:51 +0100

    Fix create template

--- create-template-vm.sh
+++ create-template-vm.sh
@@ -1,4 +1,4 @@
-qm create 200000 --name debian-12 --net0 virtio,bridge=vmbr0,tag=99,firewall=1 --scsihw virtio-scsi
+qm create 200000 --name debian-12 --net0 virtio,bridge=vmbr0,tag=99,firewall=1 --scsihw virtio-scsi-pci
 qm set 200000 --scsi0 local:0,iothread=1,backup=on,cache=writeback,format=qcow2,import-from=/root/debian-12-generic-amd64.qcow2
 qm set 200000 --boot order=scsi0
 qm set 200000 --cpu host --cores 2 --memory 2048
