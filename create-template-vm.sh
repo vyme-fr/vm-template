b2e8719c5dc8a1cd2b75193c39f53884b2226983
Savinien Petitjean <savinien.petitjean@gmail.com>
2025-12-04 17:49:42 +0100

    Add serial

--- create-template-vm.sh
+++ create-template-vm.sh
@@ -4,5 +4,7 @@ qm set 200000 --boot order=scsi0 --onboot 1
 qm set 200000 --cpu host --cores 2 --memory 2048
 qm set 200000 --ide2 local:cloudinit
 qm set 200000 --agent enabled=1,freeze-fs-on-backup=1,fstrim_cloned_disks=1
+qm set 200000 --serial 0
+qm set 200000 -vga serial0
 qm set 200000 --nameserver "45.88.180.254"
 qm template 200000
